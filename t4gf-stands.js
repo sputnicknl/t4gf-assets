7ec866ed0efdfd503e741ce18d881f033c1d6859
sputnicknl <39987960+sputnicknl@users.noreply.github.com>
2026-05-26 14:48:10 +0200

    updated list of organisations

--- t4gf-stands.js
+++ t4gf-stands.js
@@ -67,13 +67,14 @@ var O=[
   ["UWV EURES","https://www.werk.nl/eures","Entrepreneurship & Local Impact",0],
   ["Student Consultancy The Hague","https://www.studentconsultancythehague.com/","Entrepreneurship & Local Impact",0],
   ["Forerunners Consulting and Coaching","https://www.forerunnersconsulting-coaching.com","Entrepreneurship & Local Impact",1],
-  ["Stichting StepUp Team","https://www.stepupteam.nl","Entrepreneurship & Local Impact",0],
   ["Inclusion Unfolding Africa","https://inclusionunfolding.org","Democracy, Peace & Justice",0],
   ["Sky Campus Happiness","https://www.skycampushappiness.org","Entrepreneurship & Local Impact",0],
   ["Masambeni South African Internships","https://www.masambeni.com","Entrepreneurship & Local Impact",0],
   ["Move 4 Human Rights","https://justiceandpeace.nl/en/initiatives/move-4-human-rights/","Democracy, Peace & Justice",0],
   ["IdentIT","https://www.identit.eu","Tech for Good",0],
   ["Stichting Queer Mind","https://stichtingqueermind.nl","Democracy, Peace & Justice",0]
+  ["ANKA (Association of Networking Knowledge and Advancement)","https://associationanka.nl","Entrepreneurship & Local Impact",0]
+
 ];
 
 var active="all";
